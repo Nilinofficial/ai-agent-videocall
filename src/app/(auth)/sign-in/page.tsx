cd52e5a3e5a834e75e29f59a227f26ba44221d5c
Nilinofficial <itsmenilin18@gmail.com>
2025-05-29 22:13:49 +0530

    Add better auth login and signup

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInView from "@/app/modules/ui/views/SignInView";
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+function Page() {
+  return <SignInView />;
+}
+
+export default Page;
